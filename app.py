f5a53983ffe77f5178a0ed1c16ee3060f00a63c2
gvielza <voltagnelly@gmail.com>
2024-06-04 22:48:20 -0300

    sqlite

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from base_datos.conexion import Conexion
 
 app = Flask(__name__)
 
@@ -10,4 +11,11 @@ def index():
 def resultados():
     dni = request.args.get('dni')
     usuario = request.args.get('usuario')
-    return render_template('resultados.html', dni=dni, usuario=usuario)
+    contrasenna=request.args.get('password')
+    conexion=Conexion('base_datos/usuarios.db')
+    conexion.crear_tabla_cliente()
+    conexion.agregar_cliente(dni,usuario,contrasenna)
+    clientes=conexion.mostrar_clientes()
+    conexion.cerrar_conexion()
+    mensaje="Se guardo exitosamente en la base de datos"
+    return render_template('resultados.html', dni=dni, usuario=usuario, mensaje=mensaje,clientes=clientes)
